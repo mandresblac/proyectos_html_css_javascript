b36949194241cd5f8b64ef91078b8a1636c0047c
manuel <mandresblac@gmail.com>
2022-10-26 23:16:23 -0500

    commit 15

--- registroDeCalificaciones/script.js
+++ registroDeCalificaciones/script.js
@@ -5,10 +5,10 @@ function promedio() {
       const n4 = document.querySelector("#nota4").value;
       let prom = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3) + parseFloat(n4)) / 4;
       
-      if (prom <= 3) {
+      if (prom <= 2.9) {
         document.querySelector("#resultado").innerHTML = `Nota Final: ${prom.toFixed(1)} <br> REPROBADO`;
         document.querySelector("#resultado").style.color = "red";
-      }else if (prom >= 3.1 && prom <= 5) {
+      }else if (prom >= 3 && prom <= 5) {
         document.querySelector("#resultado").innerHTML = `Nota Final: ${prom.toFixed(1)} <br> APROBADO`;
         document.querySelector("#resultado").style.color = "green";
       } else {
@@ -17,12 +17,14 @@ function promedio() {
       }
     };
     
-    //Traemos del documento HTML a Javascript el elemento con id "btn" que es el input "Calcular", luego a través de addEventListener() capturamos cuando el usuario de clic sobre ese input o boton con el evento "click" como primer parámetro del método addEventListener() y como segundo parámetro pasamos una funcion flecha que ejecuta la funcion promedio() y luego resetea el formulario después de hacer click en el boton "Calcular"
+    //Traemos del documento HTML a Javascript el elemento con id "btn" que es el input "Calcular", luego a través de addEventListener() capturamos cuando el usuario de clic sobre ese input o boton con el evento "click" como primer parámetro del método addEventListener() y como segundo parámetro pasamos una funcion flecha que ejecuta la funcion promedio() y a travës del temporizador setTimeout() resetea el formulario 2 segundos (2000 milisegundos) después de hacer click en el boton "Calcular"
     document.querySelector("#btn").addEventListener("click", ()=>{
       //Llamamos a la función promedio
       promedio();
       //Traemos del documento HTML a javascript el formulario completo para después resetearlo 
       const form = document.querySelector("#formulario");
-      form.reset(); //Utilizamos .reset() para resetear toda la información del formulario (volverlo a su estado inicial vació) después de hacer click en el boton "Calcular"
+      setTimeout(() => {
+        form.reset(); //Utilizamos .reset() para resetear toda la información del formulario (volverlo a su estado inicial vació) después de hacer click en el boton "Calcular"
+      }, 2000);
     });
     
