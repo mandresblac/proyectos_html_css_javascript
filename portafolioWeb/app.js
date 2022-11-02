ac26d7ec4e383ac925a99037643c71e7180074f7
manuel blanco <mandresblac@gmail.com>
2022-11-01 20:20:18 -0500

    commit 20

---
+++ portafolioWeb/app.js
@@ -0,0 +1,63 @@
+//Funcion que carga las animaciones de las habilidades 
+const cargarAnimaciones = () => {
+  $(function(){
+    $('.chart1').easyPieChart({
+      size:160,
+      barColor:"#ff5b00",
+      scaleLength:0,
+      lineWidth:15,
+      tackColor:"#525151",
+      lineCap:"circle",
+      animate:2000
+    });
+    $('.chart2').easyPieChart({
+      size:160,
+      barColor:"#ff5b00",
+      scaleLength:0,
+      lineWidth:15,
+      tackColor:"#525151",
+      lineCap:"circle",
+      animate:2000
+    });
+    $('.chart3').easyPieChart({
+      size:160,
+      barColor:"#ff5b00",
+      scaleLength:0,
+      lineWidth:15,
+      tackColor:"#525151",
+      lineCap:"circle",
+      animate:2000
+    });
+    $('.chart4').easyPieChart({
+      size:160,
+      barColor:"#ff5b00",
+      scaleLength:0,
+      lineWidth:15,
+      tackColor:"#525151",
+      lineCap:"circle",
+      animate:2000
+    });
+    $('.chart5').easyPieChart({
+      size:160,
+      barColor:"#ff5b00",
+      scaleLength:0,
+      lineWidth:15,
+      tackColor:"#525151",
+      lineCap:"circle",
+      animate:2000
+    });
+    $('.chart6').easyPieChart({
+      size:160,
+      barColor:"#ff5b00",
+      scaleLength:0,
+      lineWidth:15,
+      tackColor:"#525151",
+      lineCap:"circle",
+      animate:2000
+    });
+  });
+} 
+
+cargarAnimaciones();
+
+
