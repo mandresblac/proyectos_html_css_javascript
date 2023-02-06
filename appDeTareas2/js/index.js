330b4d82611bb674140bd68e7861d2a293379ab7
manuel blanco <mandresblac@gmail.com>
2023-02-06 14:12:46 -0500

    commit 33

---
+++ appDeTareas2/js/index.js
@@ -0,0 +1,31 @@
+const item = document.querySelector("#todo-item");
+const resultado = document.querySelector("#resultado");
+
+//Función que nos crea la tarea
+const crearTarea = () => {
+  //Creamos elemento "li" de Html con nombre tarea
+  const tarea = document.createElement("li");
+
+  //Agregamos a la tarea el valor insertado en "item" con la propiedad textContent
+  tarea.textContent = item.value;
+
+  //Al resultado le agregamos la tarea y la retornamos
+  return resultado.appendChild(tarea);
+};
+
+document.querySelector("#btn").addEventListener("click", (event) => {
+  //Para prevenir la acción por defecto del formulario que sera procesarse
+  event.preventDefault();
+
+  //Para controlar si el usuario hace click sobre el botón sin haber insertado ningún valor en el input con id "item"
+  if (item.value === "") return false;
+  //if (item.value === "") return (resultado.innerHTML = "");
+
+  //Llamamos la función crearTarea
+  crearTarea();
+
+  //Limpiamos el input de entrada
+  item.value = "";
+  //Pone el cursor nuevamente sobe el input con id input
+  item.focus();
+});
