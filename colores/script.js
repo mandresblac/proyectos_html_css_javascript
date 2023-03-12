e5fde97528552bb30d4269a25feed210d791cf3d
manuel blanco <mandresblac@gmail.com>
2023-03-12 18:09:02 -0500

    commit 50

--- colores/script.js
+++ colores/script.js
@@ -1,1 +1,47 @@
+// ------------------ VARIABLES Y SELECTORES ------------------ //
+const blue = document.querySelector("#blue");
+const green = document.querySelector("#green");
+const yellow = document.querySelector("#yellow");
+const orange = document.querySelector("#orange");
+const red = document.querySelector("#red");
 
+const cuadro = document.querySelector("#square");
+
+// ------------------------- EVENTOS ------------------------- //
+blue.addEventListener("mouseover", () => agregarColorYMensaje("blue", "Blue"));
+blue.addEventListener("mouseout", () => quitarColorYMensaje("white"));
+
+green.addEventListener("mouseover", () =>
+  agregarColorYMensaje("green", "Green")
+);
+green.addEventListener("mouseout", () => quitarColorYMensaje("white"));
+
+yellow.addEventListener("mouseover", () =>
+  agregarColorYMensaje("yellow", "Yellow")
+);
+yellow.addEventListener("mouseout", () => quitarColorYMensaje("white"));
+
+orange.addEventListener("mouseover", () =>
+  agregarColorYMensaje("orange", "Orange")
+);
+orange.addEventListener("mouseout", () => quitarColorYMensaje("white"));
+
+red.addEventListener("mouseover", () => agregarColorYMensaje("red", "Red"));
+red.addEventListener("mouseout", () => quitarColorYMensaje("white"));
+
+// ------------------------ FUNCIONES ------------------------ //
+function agregarColorYMensaje(color, mensaje) {
+  //Agregamos color
+  cuadro.style.background = `${color}`;
+
+  //Agregamos el mensaje
+  const parrafo = document.createElement("p");
+  parrafo.textContent = `${mensaje}`;
+  parrafo.classList.add("parrafo");
+  cuadro.appendChild(parrafo);
+}
+
+function quitarColorYMensaje(color) {
+  cuadro.style.background = `${color}`;
+  cuadro.textContent = "";
+}
