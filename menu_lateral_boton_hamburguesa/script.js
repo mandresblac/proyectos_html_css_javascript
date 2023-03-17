bd32eb279a68bba304fdee7c2f070146d6940a20
manuel blanco <mandresblac@gmail.com>
2023-03-17 01:14:19 -0500

    commit 54

--- menu_lateral_boton_hamburguesa/script.js
+++ menu_lateral_boton_hamburguesa/script.js
@@ -1,12 +1,15 @@
 // ------------------ VARIABLES Y SELECTORES ------------------ //
-const btnMenu = document.querySelector("#btn-menu");
+const btnMenu1 = document.querySelector("#btn-menu");
+const btnMenu2 = document.querySelector("#btn-menu2");
 const menu = document.querySelector("#menu");
 
 // ------------------------- EVENTOS ------------------------- //
 function eventListeners() {
-  btnMenu.addEventListener("click", mostrarMenu);
-  btnMenu.addEventListener("mouseover", () => ponerFondo("lightgray"));
-  btnMenu.addEventListener("mouseout", () => quitarFondo("white"));
+  btnMenu1.addEventListener("click", mostrarMenu);
+  btnMenu1.addEventListener("mouseover", () => ponerFondo("lightgray"));
+  btnMenu1.addEventListener("mouseout", () => quitarFondo("white"));
+
+  btnMenu2.addEventListener("click", ocultarMenu);
 }
 
 eventListeners();
@@ -15,23 +18,40 @@ eventListeners();
 function mostrarMenu() {
   if (menu.style.display === "none") {
     menu.style.display = "block";
-    /* btnMenu.style.display = "none"; */
+    btnMenu1.style.display = "none";
     document.querySelector(".text-menu").style.display = "none";
-    /* botonMenu(); */
   } else {
     menu.style.display = "none";
     document.querySelector(".text-menu").style.display = "block";
   }
 }
 
+function ocultarMenu() {
+  if(menu.style.display === "block") {
+    menu.style.display = "none";
+    btnMenu1.style.display = "block";
+    document.querySelector(".text-menu").style.display = "block";
+    btnMenu2.onmouseover = () => ponerFondo("lightgray");
+    btnMenu2.onmouseout = () => quitarFondo("white");
+    document.querySelector("#btn-menu2 i").style.color = "black";
+  } else {
+    menu.style.display = "block";
+    document.querySelector(".text-menu").style.display = "none";
+    btnMenu1.onmouseover = () => ponerFondo("lightgray");
+    btnMenu1.onmouseout = () => quitarFondo("white");
+  }
+}
+
 function ponerFondo(color) {
-  btnMenu.style.background = `${color}`;
-  btnMenu.style.border = "none";
-  btnMenu.style.borderRadius = "50%";
+  btnMenu1.style.background = `${color}`;
+  btnMenu2.style.background = `${color}`;
+  btnMenu1.style.borderRadius = "50%";
+  btnMenu2.style.borderRadius = "50%";
 }
 
 function quitarFondo(color) {
-  btnMenu.style.background = `${color}`;
+  btnMenu1.style.background = `${color}`;
+  btnMenu2.style.background = `${color}`;
 }
 
 /* function botonMenu() {
