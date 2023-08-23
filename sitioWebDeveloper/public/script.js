3460196151dce4e0d5aebcd4338da96b8f8a2a96
manuel blanco <mandresblac@gmail.com>
2023-08-22 23:10:09 -0500

    commit 88

--- sitioWebDeveloper/public/script.js
+++ sitioWebDeveloper/public/script.js
@@ -1,13 +1,18 @@
 const boton = document.querySelector("#btn-menu");
 const menu = document.querySelector("#menu");
+const header = document.querySelector("#header");
 
 boton.addEventListener("click", (e) => {
-  e.preventDefault();
+  console.log(e.target);
+  console.log(e.target.className);
 
   if (menu.style.display === "none") {
     menu.style.display = "block";
-    menu.style.display = "none";
+    header.style.display = "flex";
+    header.style.flexDirection = "column";
+    boton.style.alignSelft = "flex-end";
+    menu.style.order = 2;
   } else {
-    menu.style.display = "block";
+    menu.style.display = "none";
   }
 });
