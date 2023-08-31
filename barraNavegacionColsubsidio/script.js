23074d0fc3a901e8da3199ef21a39b501e7dd97b
manuel blanco <mandresblac@gmail.com>
2023-08-31 00:16:56 -0500

    commit 106

---
+++ barraNavegacionColsubsidio/script.js
@@ -0,0 +1,6 @@
+let btnMenu = document.querySelector("#btn-menu");
+let menu = document.querySelector("#menu");
+
+btnMenu.addEventListener("click", function () {
+  menu.classList.toggle("mostrar");
+});
