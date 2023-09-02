10a548069a051dfe16c8a103590c24caf734faef
manuel blanco <mandresblac@gmail.com>
2023-09-01 23:54:29 -0500

    commit 110

--- paginaWebCentroEducativo/js/script.js
+++ paginaWebCentroEducativo/js/script.js
@@ -1,2 +1,6 @@
 const btnMenu = document.querySelector("#btn-menu");
 const nav = document.querySelector("#nav");
+
+btnMenu.addEventListener("click", (e) => {
+  nav.classList.toggle("mostrar");
+});
