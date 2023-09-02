561499edf3177da902bb3976631ae3015d99379f
manuel blanco <mandresblac@gmail.com>
2023-09-02 17:32:55 -0500

    commit 112

--- paginaWebCentroEducativo/js/script.js
+++ paginaWebCentroEducativo/js/script.js
@@ -1,6 +1,20 @@
 const btnMenu = document.querySelector("#btn-menu");
+const btnClose = document.querySelector("#btn-close");
 const nav = document.querySelector("#nav");
 
-btnMenu.addEventListener("click", (e) => {
-  nav.classList.toggle("mostrar");
+btnMenu.addEventListener("click", () => {
+  if (nav.classList.toggle("mostrar-nav")) {
+    btnMenu.classList.toggle("ocultar-btn-menu");
+    btnClose.classList.toggle("mostrar-btn-menu");
+  }
+});
+
+btnClose.addEventListener("click", () => {
+  if (nav.classList.toggle("mostrar-nav")) {
+    btnClose.classList.toggle("mostrar-btn-menu");
+    btnMenu.classList.toggle("ocultar-btn-menu");
+  } else {
+    btnClose.classList.toggle("mostrar-btn-menu");
+    btnMenu.classList.toggle("ocultar-btn-menu");
+  }
 });
