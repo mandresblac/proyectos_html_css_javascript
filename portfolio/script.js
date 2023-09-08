274457373faf0c4f2060baa47c18f907783ee0ba
manuel blanco <mandresblac@gmail.com>
2023-09-07 23:29:22 -0500

    commit 119

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,22 +1,52 @@
 const btnMenu = document.querySelector("#btn-menu");
 const btnClose = document.querySelector("#btn-cancel");
 const nav = document.querySelector("#nav");
+const btnDarkTheme = document.querySelector("#btn-dark-theme");
+const btnlightTheme = document.querySelector("#btn-light-theme");
 
+/* Eventos para mostrar menu de na vegacion */
 btnMenu.addEventListener("click", () => {
-  // nav.classList.toggle("mostrar-nav");
   if (nav.classList.toggle("mostrar-nav")) {
-    btnMenu.classList.toggle("ocultar-btn-menu");
-    btnClose.classList.toggle("mostrar-btn-menu");
-    // btnMenu.classList.toggle("mostrar-btn-menu");
+    btnMenu.classList.toggle("ocultar-btn");
+    btnClose.classList.toggle("mostrar-btn");
   }
 });
 
 btnClose.addEventListener("click", () => {
   if (nav.classList.toggle("mostrar-nav")) {
-    btnClose.classList.toggle("mostrar-btn-menu");
-    btnMenu.classList.toggle("ocultar-btn-menu");
+    btnClose.classList.toggle("mostrar-btn");
+    btnMenu.classList.toggle("ocultar-btn");
   } else {
-    btnClose.classList.toggle("mostrar-btn-menu");
-    btnMenu.classList.toggle("ocultar-btn-menu");
+    btnClose.classList.toggle("mostrar-btn");
+    btnMenu.classList.toggle("ocultar-btn");
+  }
+});
+
+/* Evento para cambiar de modo claro a oscuro */
+btnDarkTheme.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+  document.body.classList.toggle("dark-theme");
+
+  // Para cambiar el icono del boton
+  const className = document.body.className;
+  if (className === "light-theme") {
+    // btnDarkTheme.classList.toggle("mostrar-btn-light");
+    btnlightTheme.classList.toggle("ocultar-btn");
+  } else {
+    btnlightTheme.classList.toggle("mostrar-btn-light");
+    btnDarkTheme.classList.toggle("ocultar-btn");
+  }
+});
+
+btnlightTheme.addEventListener("click", () => {
+  document.body.classList.toggle("light-theme");
+  document.body.classList.toggle("dark-theme");
+
+  const className = document.body.className;
+  if (className === "dark-theme") {
+    btnDarkTheme.classList.toggle("ocultar-btn");
+  } else {
+    btnlightTheme.classList.toggle("mostrar-btn-light");
+    btnDarkTheme.classList.toggle("ocultar-btn");
   }
 });
