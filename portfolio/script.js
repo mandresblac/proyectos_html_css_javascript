0b695478793c91dcd609f7f873fdbe030198243a
manuel blanco <mandresblac@gmail.com>
2023-09-07 22:18:52 -0500

    commit 118

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,1 +1,22 @@
-console.log("Hola mundill0");
+const btnMenu = document.querySelector("#btn-menu");
+const btnClose = document.querySelector("#btn-cancel");
+const nav = document.querySelector("#nav");
+
+btnMenu.addEventListener("click", () => {
+  // nav.classList.toggle("mostrar-nav");
+  if (nav.classList.toggle("mostrar-nav")) {
+    btnMenu.classList.toggle("ocultar-btn-menu");
+    btnClose.classList.toggle("mostrar-btn-menu");
+    // btnMenu.classList.toggle("mostrar-btn-menu");
+  }
+});
+
+btnClose.addEventListener("click", () => {
+  if (nav.classList.toggle("mostrar-nav")) {
+    btnClose.classList.toggle("mostrar-btn-menu");
+    btnMenu.classList.toggle("ocultar-btn-menu");
+  } else {
+    btnClose.classList.toggle("mostrar-btn-menu");
+    btnMenu.classList.toggle("ocultar-btn-menu");
+  }
+});
