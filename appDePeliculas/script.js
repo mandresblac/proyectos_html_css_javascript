9c727cc8785a7e1dd34cdfffbda10c49d8aa4998
manuel blanco <mandresblac@gmail.com>
2023-10-26 18:59:21 -0500

    commit 144

--- appDePeliculas/script.js
+++ appDePeliculas/script.js
@@ -4,34 +4,51 @@ const listaPeliculas = [
     nombre: "Harry Potter",
     imagen:
       "https://hips.hearstapps.com/hmg-prod/images/harry-potter-y-la-piedra-filosofal-1549965998.jpg",
-    url: "https://www.google.com",
+    url: "https://www.youtube.com/embed/WE4AJuIvG1Y?si=PuhSezD6QwQy8urw",
   },
   {
     nombre: "Barbie",
     imagen:
       "https://elcomercio.pe/resizer/sjdQp0NvLtsRndrAP2Ec05gQcho=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/WZTVEL3PIBA4JPH4XZMIVP2EUM.jpg",
-    url: "https://www.google.com",
+    url: "https://www.youtube.com/embed/pBk4NYhWNMM?si=eVOL7brRuoPl-GCN",
   },
   {
     nombre: "Star Wars",
     imagen:
       "https://hips.hearstapps.com/hmg-prod/images/la-amenaza-fantasma-1575448990.jpg",
-    url: "https://www.google.com",
+    url: "https://www.youtube.com/embed/n1CUHjrc9Sc?si=xLS_V59Q5tCLx_a5",
   },
   {
     nombre: "El señor de los anillos",
     imagen: "https://es.web.img3.acsta.net/pictures/22/10/04/16/26/1929878.jpg",
-    url: "https://www.google.com",
+    url: "https://www.youtube.com/embed/19nt1L8nLrA?si=aSRWS0-l_I18WHKY",
   },
   {
     nombre: "Volver al futuro",
     imagen: "https://www.tematika.com/media/catalog/Ilhsa/Imagenes/638724.jpg",
-    url: "https://www.google.com",
+    url: "https://www.youtube.com/embed/GM6_MHRc4Xo?si=oVkkCH4XHK2LIkm8",
   },
   {
     nombre: "Forrest Gump",
     imagen: "https://pics.filmaffinity.com/Forrest_Gump-212765827-large.jpg",
-    url: "https://www.google.com",
+    url: "https://www.youtube.com/embed/bLvqoHBptjg?si=I2RvQsMtzGeEWbJG",
+  },
+  {
+    nombre: "Titanic",
+    imagen:
+      "https://r3.abcimg.es/resizer/resizer.php?imagen=https%3A%2F%2Fs1.abcstatics.com%2Fmedia%2Fpeliculas%2F000%2F044%2F428%2Ftitanic-1.jpg",
+    url: "https://www.youtube.com/embed/CHekzSiZjrY?si=USo1dtghWp1bRnAn",
+  },
+  {
+    nombre: "The incredibles",
+    imagen: "https://pics.filmaffinity.com/Los_Increaibles-939387273-large.jpg",
+    url: "https://www.youtube.com/embed/-UaGUdNJdRQ?si=gkD_JdbNXY-0n3YX",
+  },
+  {
+    nombre: "Top Gun: Maverick",
+    imagen:
+      "https://pics.filmaffinity.com/Top_Gun_Maverick-733802541-large.jpg",
+    url: "https://www.youtube.com/embed/qSqVVswa420?si=UcTt_-k2HPW7AMpu",
   },
 ];
 
@@ -59,7 +76,8 @@ listaPeliculas.forEach((pelicula) => {
   // Agregamos el elemento figure al elemento a
   enlace.appendChild(figure);
   enlace.classList.add("enlace");
-  enlace.href = listaPeliculas.url;
+  // enlace.href = pelicula.url;
+  enlace.setAttribute("href", `${pelicula.url}`);
   enlace.target = "_blank";
 
   // Agregamos elemento a al contenedor en ele HTML
